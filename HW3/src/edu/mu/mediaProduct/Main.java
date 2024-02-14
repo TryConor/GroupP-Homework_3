8f000db92bb49832a27a6776c11d860435cd1961
TryConor <cjmw5t@umsystem.edu>
2024-02-14 17:23:46 -0600

    Changes to Main,Vinyl,Tape,CD, and MediaProduct
    Ensured that all classes inherited from MediaProduct. Added Constructors and Copy Constructors to all classes. Added Getters, Setters, and toString to MediaProduct. Added sample output to main.

--- HW3/src/edu/mu/mediaProduct/Main.java
+++ HW3/src/edu/mu/mediaProduct/Main.java
@@ -1,9 +1,13 @@
 package edu.mu.mediaProduct;
 
+import edu.mu.mediaProduct.products.Genre;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MediaProduct product = new MediaProduct("Movie", 19.99, 2023, Genre.ROCK);
+        System.out.println(product);
 
 	}
 
