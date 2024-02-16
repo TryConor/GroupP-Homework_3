812b731be281d6b2eb19f2f91fd88cbd09b645b4
MichaelKeeney <112013713+MichaelKeeney@users.noreply.github.com>
2024-02-16 09:10:55 -0600

    Start of initializeStock()
    I began work on the initializeStock method.

--- HW3/src/edu/mu/StockManagerSingleton/StockManagerSingleton.java
+++ HW3/src/edu/mu/StockManagerSingleton/StockManagerSingleton.java
@@ -1,11 +1,16 @@
 package edu.mu.StockManagerSingleton;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import edu.mu.mediaProduct.MediaProduct;
+
 public class StockManagerSingleton {
 	
 	private static StockManagerSingleton instance = null;
 	
 	private final static String filePath = "files/inventory.csv";
-	
+		
 	private StockManagerSingleton() {
 		
 	}
@@ -17,4 +22,16 @@ public class StockManagerSingleton {
 		return instance;
 	}
 	
+	ArrayList<MediaProduct> inventory = new ArrayList<MediaProduct>();
+	
+	public boolean initializeStock() {
+		try {
+			
+			return true;
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
